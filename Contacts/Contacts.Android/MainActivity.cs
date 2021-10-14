c422cd55772e899a93ca8a9c4937d70ec8ea4152
Sviatoslav Maniuk <sviatuniversity2019@gmail.com>
2021-10-15 02:55:44 +0300

    "Updated SignInViewModel (SignIn button is disabled when Login or Password is empty; added alert). Disabled landscape orientation"

--- Contacts/Contacts.Android/MainActivity.cs
+++ Contacts/Contacts.Android/MainActivity.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using Acr.UserDialogs;
 using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
@@ -7,12 +7,13 @@ using Android.OS;
 
 namespace Contacts.Droid
 {
-    [Activity(Label = "Contacts", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Label = "Contacts", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ScreenOrientation = ScreenOrientation.Portrait, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            UserDialogs.Init(this);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
--- Contacts/Contacts/ViewModel/SignInViewModel.cs
+++ Contacts/Contacts/ViewModel/SignInViewModel.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel;
 using System.Windows.Input;
+using Acr.UserDialogs;
 using Contacts.View;
 using Prism.Commands;
 using Prism.Mvvm;
@@ -21,11 +22,17 @@ namespace Contacts.ViewModel
         }
         
         #region --- Public Properties ---
-
+        
         public ICommand SignUpLabelTapCommand => new Command(OnSignUpLabelTap);
 
-        public ICommand SignInButtonTapCommand =>
-            new DelegateCommand(OnSignInButtonTap).ObservesCanExecute(() => AllFieldsNotEmpty());
+        public ICommand SignInButtonTapCommand => new DelegateCommand(OnSignInButtonTap).ObservesCanExecute(()=>AllFieldsNotNull);
+        
+        private bool _allFieldsNotNull;
+        public bool AllFieldsNotNull
+        {
+            get => _allFieldsNotNull;
+            set => SetProperty(ref _allFieldsNotNull, value);
+        }
         
         public string Login
         {
@@ -42,23 +49,37 @@ namespace Contacts.ViewModel
         #endregion
 
         #region --- Overrides ---
+        
+        protected override void OnPropertyChanged(PropertyChangedEventArgs args)
+        {
+            base.OnPropertyChanged(args);
 
+            switch (args.PropertyName)
+            {
+                case nameof(Login):
+                case nameof(Password):
+                    AllFieldsNotNull = !(string.IsNullOrWhiteSpace(Login) || string.IsNullOrWhiteSpace(Password));
+                    break;
+            }
+        }
         #endregion
  
         #region --- Private Halpers ---
 
         private async void OnSignInButtonTap()
         {
-            await _navigationService.NavigateAsync("/"+nameof(MainList));
+            if (false)
+            {
+                await _navigationService.NavigateAsync("/NavigationPage/"+nameof(MainList));
+            }
+            else
+            {
+                UserDialogs.Instance.Alert("Invalid login or password!");
+            }
         }
         private async void OnSignUpLabelTap()
         {
             await _navigationService.NavigateAsync(nameof(SignUp));
-        }
-
-        private bool AllFieldsNotEmpty()
-        {
-            return !(string.IsNullOrEmpty(Login) || string.IsNullOrEmpty(Password));
         }
 
         #endregion
